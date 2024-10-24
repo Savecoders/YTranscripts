3c0792f7c72a194f1a779ccd06affd5842d1c824
Savecoders <pablopincay2003@gmail.com>
2024-10-24 16:40:38 -0500

    feat: get txt with transcript video

---
+++ src/services/transcript.service.ts
@@ -0,0 +1,73 @@
+import BrowserAdapter from './browser.adapter';
+
+class TranscriptService {
+  private readonly browser: BrowserAdapter<BrowserTab>;
+
+  constructor(browserAdapter: BrowserAdapter<BrowserTab>) {
+    this.browser = browserAdapter;
+  }
+
+  async getUrlBroswerTab(): Promise<string | undefined> {
+    const tab = await this.browser.getBrowserTab();
+    return tab.url;
+  }
+
+  async isYoutubeTab(): Promise<boolean | undefined> {
+    const url = await this.getUrlBroswerTab();
+    return url?.includes('youtube.com');
+  }
+
+  async transcriptionVideo(): Promise<string | undefined> {
+    if (!(await this.isYoutubeTab())) {
+      return;
+    }
+    await this.browser.executeScript(async () => {
+      // DOM tab selectors
+      const ytTranscriptSectionStyle =
+        'style-scope ytd-video-description-transcript-section-renderer';
+      const transcriptSegmentsContainerID = 'segments-container';
+
+      // get transcript s
+      const transcriptSectionAviable = document.getElementsByClassName(ytTranscriptSectionStyle);
+
+      if (!transcriptSectionAviable) {
+        return;
+      }
+
+      const trasncriptSectionContent = [...transcriptSectionAviable];
+
+      trasncriptSectionContent.forEach(el => {
+        // click button in transcript section
+        if (el.tagName === 'YTD-BUTTON-RENDERER') {
+          el.getElementsByTagName('button')[0].click();
+        }
+      });
+
+      // wait for transcript to load
+      await new Promise(resolve => setTimeout(resolve, 3000));
+
+      const transcriptSegmentsContainer = document.getElementById(transcriptSegmentsContainerID);
+
+      if (!transcriptSegmentsContainer) {
+        return;
+      }
+
+      const transcriptSegments = [...transcriptSegmentsContainer.children];
+
+      const transcriptSegmentsText = transcriptSegments.map(el => {
+        // el is div and need get yt-formatted-string tag
+        const ytFormattedString = el.getElementsByTagName('yt-formatted-string');
+        if (ytFormattedString.length > 0) {
+          const transcription = `${ytFormattedString[0].innerHTML}`;
+          return transcription;
+        }
+      });
+
+      const transcript = transcriptSegmentsText.join(' ');
+      return transcript;
+    });
+    return;
+  }
+}
+
+export default TranscriptService;
