82a00b47fe6f65c7e7236e6765b1c4434fe6408c
Savecoders <pablopincay2003@gmail.com>
2024-10-28 10:53:57 -0500

    feat: new component clip transcription

---
+++ src/components/ui/clip-transcription.tsx
@@ -0,0 +1,40 @@
+import { ClipboardRoot, Stack } from '@chakra-ui/react';
+import { ClipboardIconButton } from './clipboard';
+import { CSSProperties } from 'react';
+
+interface ClipTranscriptionProps {
+  textTranscription: string;
+  style?: CSSProperties;
+}
+
+export function ClipTranscription({ textTranscription, style }: ClipTranscriptionProps) {
+  return (
+    <ClipboardRoot maxW='400px' value={textTranscription || ''}>
+      <Stack
+        direction='row'
+        borderRadius={4}
+        p={4}
+        borderRightStyle={'solid'}
+        borderWidth={1}
+        whiteSpace={'wrap'}
+        overflow={'hidden'}
+        textOverflow={'ellipsis'}
+        style={style}
+      >
+        <div
+          style={{
+            width: '100%',
+            maxWidth: '400px',
+            overflowX: 'hidden',
+            overflowY: 'auto',
+            maxHeight: '100px',
+            scrollbarWidth: 'none',
+          }}
+        >
+          <p>{textTranscription}</p>
+        </div>
+        <ClipboardIconButton me='-2' />
+      </Stack>
+    </ClipboardRoot>
+  );
+}
