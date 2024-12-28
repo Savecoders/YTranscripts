bbc9e58e0aac5336403fe696ce857cd50183f226
Savecoders <pablopincay2003@gmail.com>
2024-12-28 16:08:21 -0500

    chore: remove orther agents browser

--- src/utils/detectBrowser.ts
+++ src/utils/detectBrowser.ts
@@ -1,6 +1,6 @@
 export const detectBrowser = (): browsersName => {
   const userAgent = navigator.userAgent.toLowerCase();
-  if (userAgent.includes('chrome') && !userAgent.includes('opera') && !userAgent.includes('opr')) {
+  if (userAgent.includes('chrome')) {
     return 'chrome'; // Incluye Chrome, Edge, Brave
   } else if (userAgent.includes('firefox')) {
     return 'firefox';
