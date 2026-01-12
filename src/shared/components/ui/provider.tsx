36121b218624c1fac4ea74d84f91ff8f66261450
Savecoders <pablopincay2003@gmail.com>
2026-01-12 15:01:54 -0500

    feat: define toast and install provider

--- src/shared/components/ui/provider.tsx
+++ src/shared/components/ui/provider.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { ColorModeProvider } from './color-mode';
+import { Toaster } from './toaster';
 
 export function Provider(props: React.PropsWithChildren) {
   return (
     <ChakraProvider value={defaultSystem}>
       <ColorModeProvider>{props.children}</ColorModeProvider>
+      <Toaster />
     </ChakraProvider>
   );
 }
